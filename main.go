be454ee4935e6b09b7de21931b2d642fb9957fa0
thiago <thiago@tvidal.uk>
2022-10-26 11:49:45 +0100

    reduce brightness on shutdown

--- main.go
+++ main.go
@@ -202,6 +202,9 @@ func closeDevice(dev *streamdeck.Device) {
 	if err := dev.Clear(); err != nil {
 		fmt.Fprintln(os.Stderr, "Unable to clear the Stream Deck")
 	}
+	if err := dev.Sleep(); err != nil {
+		fmt.Fprintln(os.Stderr, "Unable to sleep the Stream Deck")
+	}
 	if err := dev.Close(); err != nil {
 		fmt.Fprintln(os.Stderr, "Unable to close Stream Deck")
 	}
