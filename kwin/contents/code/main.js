b5e08f5ce83030de614235dac6c4eb0a753f3b01
thiago <thiago@tvidal.uk>
2025-07-10 18:42:44 +0100

    make kwin script kde5 compatible

--- kwin/contents/code/main.js
+++ kwin/contents/code/main.js
@@ -5,9 +5,11 @@ const method = "ActiveWindowChanged";
 function activeWindowChanged(window) {
     const name = window.resourceName + "." + window.resourceClass;
     const id = window.id.toString();
-    print("activated: " + name + ' ' + id);
+    print("windowActivated: " + name + ' ' + id);
     callDBus(service, path, service, method, name, id);
 }
 
-workspace.windowActivated.connect(activeWindowChanged);
-print("activated: DeckMaster");
+const signal = workspace.windowActivated ?? workspace.clientActivated
+signal.connect(activeWindowChanged);
+
+print("enabled: DeckMaster");
